e7d29bc17142a4eacbec1422ee452acd31779028
juan0p123 <juanoportus@alu.unach.cl>
2026-03-31 09:27:00 -0300

    Jest, calculadora simple

---
+++ src/kirchhoff.js
@@ -0,0 +1,5 @@
+function calcularVoltajeFaltante(v1, v2) {
+  return -(v1 + v2);
+}
+
+module.exports = { calcularVoltajeFaltante };
---
+++ tests/kirchhoff.test.js
@@ -0,0 +1,9 @@
+const { calcularVoltajeFaltante } = require('../src/kirchhoff.js');
+
+test('calcula correctamente el voltaje faltante', () => {
+  expect(calcularVoltajeFaltante(5, 3)).toBe(-8);
+});
+
+test('funciona con valores negativos', () => {
+  expect(calcularVoltajeFaltante(-2, 4)).toBe(-2);
+});
