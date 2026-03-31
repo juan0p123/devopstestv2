8fda9140a93b9073f32ecc4b1f7d47490a54255d
juan0p123 <juanoportus@alu.unach.cl>
2026-03-31 09:59:44 -0300

    Mensaje

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginVue.configs["flat/essential"],
-]);
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+    },
+    rules: {
+      semi: "error",
+      quotes: ["error", "double"],
+    },
+  },
+
+  ...pluginVue.configs["flat/essential"],
+];
